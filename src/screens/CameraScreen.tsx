8c41cecc0706611cef5a3190e2cfae558f564c12
apollosol13 <bstudenc@gmail.com>
2026-01-17 19:29:26 -0500

    Remove duplicate closing TouchableOpacity tag

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -326,7 +326,6 @@ export default function CameraScreen() {
               )}
             </TouchableOpacity>
           </View>
-              </TouchableOpacity>
             </>
           )}
         </View>
